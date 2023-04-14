1b55c9cd29da5b6bc7fe19ba3e16a288f7f523f7
chk7082 <whgusrl7082@gmail.com>
2023-04-14 15:30:38 +0900

    comment

--- movies/urls.py
+++ movies/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/update/', views.update, name='update'),
+
+    path('<pk>/comments/', views.comments_create, name='comments_create'),
+    path('<movie_pk>/comments/<comment_pk>/delete/', views.comments_delete, name='comments_delete'),
+    path('<pk>/likes/', views.likes, name='likes'),
 ]
